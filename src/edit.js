a28f79bdcbd5b3dc97a4f52caf4bed0ad612b7b3
Mariusz von Sternenwerft <marius@sternenwerft.com>
2024-07-16 16:06:14 +0200

    example box done2

--- src/edit.js
+++ src/edit.js
@@ -1,41 +1,73 @@
-/**
- * Retrieves the translation of text.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
- */
 import { __ } from '@wordpress/i18n';
-
-/**
- * React hook that is used to mark the block wrapper element.
- * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
- */
-import { useBlockProps } from '@wordpress/block-editor';
-
-/**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * Those files can contain any CSS code that gets applied to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
- */
+import { useBlockProps, RichText, MediaUpload, MediaUploadCheck, InspectorControls } from '@wordpress/block-editor';
+import { PanelBody, Button, TextControl } from '@wordpress/components';
 import './editor.scss';
 
-/**
- * The edit function describes the structure of your block in the context of the
- * editor. This represents what the editor will render when the block is used.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
- *
- * @return {Element} Element to render.
- */
-export default function Edit() {
-	return (
-		<p { ...useBlockProps() }>
-			{ __(
-				'Dynamic Test Block – hello from the editor!',
-				'example-dynamic'
-			) }
-		</p>
-	);
+export default function Edit({ attributes, setAttributes }) {
+  const { heading, text, imageUrl, imageLink } = attributes;
+
+  const onChangeHeading = (newHeading) => {
+    setAttributes({ heading: newHeading });
+  };
+
+  const onChangeText = (newText) => {
+    setAttributes({ text: newText });
+  };
+
+  const onSelectImage = (media) => {
+    setAttributes({
+      imageUrl: media.url,
+    });
+  };
+
+  const onChangeImageLink = (newLink) => {
+    setAttributes({ imageLink: newLink });
+  };
+
+  return (
+    <div {...useBlockProps()}>
+      <InspectorControls>
+        <PanelBody title={__('Image Settings', 'your-text-domain')}>
+          <MediaUploadCheck>
+            <MediaUpload
+              onSelect={onSelectImage}
+              allowedTypes={['image']}
+              render={({ open }) => (
+                <Button onClick={open}>{__('Choose Image', 'your-text-domain')}</Button>
+              )}
+            />
+          </MediaUploadCheck>
+          <TextControl
+            label={__('Image Link', 'your-text-domain')}
+            value={imageLink}
+            onChange={onChangeImageLink}
+          />
+        </PanelBody>
+      </InspectorControls>
+      <div className='testblock'>
+        <div className="testblock__left">
+          {imageUrl && (
+              <img src={imageUrl} alt="" />
+          )}
+        </div>
+        <div className="testblock__right">
+          <RichText
+            tagName="h2"
+            onChange={onChangeHeading}
+            value={heading}
+            placeholder={__('Add Heading...', 'your-text-domain')}
+          />
+          <RichText
+            tagName="p"
+            onChange={onChangeText}
+            value={text}
+            placeholder={__('Write your text...', 'your-text-domain')}
+          />
+        </div>
+        <div className="testblock__footer">
+          &copy; {new Date().toLocaleString('default', { hour: '2-digit', minute: '2-digit', month: 'long', year: 'numeric' })}
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/render.php
+++ src/render.php
@@ -2,7 +2,29 @@
 /**
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
+
+global $post;
+
+$heading = $attributes['heading'];
+$text = $attributes['text'];
+$image_url = $attributes['imageUrl'];
+$image_link = $attributes['imageLink'];
+$current_time = date('H:i, F d Y');
 ?>
-<p <?php echo get_block_wrapper_attributes(); ?>>
-	<?php esc_html_e( 'Dynamic Test Block – hello from a dynamic block!', 'example-dynamic' ); ?>
-</p>
+
+<div <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>>
+	<div class="testblock">
+		<div class="testblock__left">
+			<a href="<?php echo esc_url($image_link); ?>">
+				<img src="<?php echo esc_url($image_url); ?>" alt="">
+			</a>
+		</div>
+		<div class="testblock__right">
+			<h2><?php echo esc_html($heading); ?></h2>
+			<p><?php echo wp_kses_post($text); ?></p>
+		</div>
+		<div class="testblock__footer">
+			&copy; <?php echo $current_time; ?>
+		</div>
+	</div>
+</div>
